558be8c58d7e0e72d6a18ef667b18a6fd1aa5894
Nguyen Quoc Bao <130636520+quocbao1902@users.noreply.github.com>
2023-06-15 14:02:23 +0900

    fukuv0615

--- fukuv0615/Form1.cs
+++ fukuv0615/Form1.cs
@@ -2,9 +2,24 @@ namespace fukuv0615
 {
     public partial class Form1 : Form
     {
+        int ans = rand.Next(0, 100);
+        int count = rand.Next(0);
+        int guess = 0;
+        static Random rand = new Random();
         public Form1()
+
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            MessageBox.Show($"{count}");
+            if ((guess == count) && (count == ans)) 
+            {
+                MessageBox.Show("bingo");
+            }
         }
     }
 }
