c4a8e0d3c445c14bb762c56d3ead9c6e003c3b2c
Nguyen Quoc Bao <130636520+quocbao1902@users.noreply.github.com>
2023-06-15 14:09:00 +0900

    fukuv0615

--- fukuv0615/Form1.cs
+++ fukuv0615/Form1.cs
@@ -3,8 +3,7 @@ namespace fukuv0615
     public partial class Form1 : Form
     {
         int ans = rand.Next(0, 100);
-        int count = rand.Next(0);
-        int guess = 0;
+        int count = 0;
         static Random rand = new Random();
         public Form1()
 
@@ -15,11 +14,18 @@ namespace fukuv0615
         private void button1_Click(object sender, EventArgs e)
         {
             count++;
-            MessageBox.Show($"{count}");
-            if ((guess == count) && (count == ans)) 
+            MessageBox.Show(count.ToString() + "‰ñ–Ú‚Ì’§í");
+            int guess = int.Parse(textBox1.Text);
+            if (ans == guess)
             {
-                MessageBox.Show("bingo");
+                MessageBox.Show("Bingo!!");
+                count = 0;
+                ans = rand.Next(101);
             }
+            else if (ans < guess)
+                MessageBox.Show("Less");
+            else
+                MessageBox.Show("Greater");
         }
     }
 }
